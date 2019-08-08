e9dba7bb82f11ad7e6532d70ee4b26bf453f6ed1
Maxime Verstraeten <v.maxime@gmail.com>
2019-08-08 15:15:25 +0100

    Add files via upload

---
+++ PS2Mouse/PS2Mouse.ino
@@ -0,0 +1,109 @@
+/*
+  PS2 Pinout
+  1 : Data
+  3 : GND
+  4 : VCC
+  5 : CLK
+*/
+
+//representation : mouse moves x and y from normal view -> 2D polar coordinates (r = sqrt(x²+y²), theta = atan(y/x))
+// mouse turns around = phi -> angular offset of polar in its reference
+
+#include "PS2Mouse.h"
+#include "coordinates.h"
+
+#define PITCH 0.00318 //resolution in cm of the optical mouse
+              // we can get an 8-bit (127 values) per communication. 
+              //max speed therefore depends on communication speed -> oscilloscope clock 12.85kHz, actual data rate in this code: 59Hz
+              // max speed = 59*127*0.00318 = 24cm/sec
+
+PS2Mouse mouse(6, 5); //clk, data
+PS2Mouse mouse2(3, 2); //clk data for mouse 2
+
+float posx = 0;
+float posy = 0;
+float posy2 = 0;
+float posz = 0;
+
+float previousx = posx;
+float previousy = posy;
+float previousy2 = posy2;
+float previousz = posz;
+
+float vector[2];
+
+int x, y, x2, z;
+
+bool rotation = true;
+bool polar = true;
+
+Coordinates polarCoord = Coordinates();
+
+void setup() {
+  Serial.begin(500000);
+  while (!Serial);
+  Serial.print("Setup...");
+
+  mouse.begin();
+  mouse2.begin();
+  Serial.println("complete!");
+}
+
+void loop() {
+  uint8_t stat, stat2;
+  mouse.getPosition(stat, x, y);
+  mouse2.getPosition(stat2, x2, z);
+  
+  previousx = posx;
+  previousy = posy;
+  previousy2 = posy2;
+  previousz = posz;
+
+  posz = previousz + (float)z;
+
+  if (rotation) {
+    toVector(x, y, posz); //rotation of x and y depending on z reference
+    x = vector[0];
+    y = vector[1];
+  }
+  posx = previousx + (float)x;
+  posy = previousy + (float)y;
+  posy2 = previousy2 + (float)x2; //in its reference, x of mouse2 is y in mouse1.
+  
+  Serial.print(posx);
+  Serial.print(" ");
+  Serial.print(posy);
+  Serial.print(" ");
+  Serial.print(posz);
+  Serial.print(" ");
+  Serial.println("end");
+
+/*
+  Serial.print("distance parcourue : ");
+  Serial.println(PITCH*posx);
+*/
+
+  if (polar) {
+    //go from cart to polar coordinates
+    polarCoord.fromCartesian(x, y);
+    /*
+    Serial.print("polar coord : ");
+    Serial.print(polarCoord.getR());
+    Serial.print(" ");
+    Serial.println(polarCoord.getAngle() * (360 / (2 * PI)));
+  */
+  }
+}
+
+
+void toVector(float x, float y, float z) {
+  //rotates the vector around the z axis to follow the path
+  float rotatedX = (float) (x * cos(z / 360) - y * sin(z / 360));
+  float rotatedY = (float) (x * sin(z / 360) + y * cos(z / 360));
+  float rotatedVector[2];
+  vector[0] = rotatedX;
+  vector[1] = rotatedY;
+}
+
+
+
---
+++ ProcessingMouseSignal/ProcessingMouseSignal.pde
@@ -0,0 +1,100 @@
+/**
+ * Mouse Signals. 
+ * 
+ * Move and click the mouse to generate signals. 
+ * The top row is the signal from "mouseX", 
+ * the middle row is the signal from "mouseY",
+ * and the bottom row is the signal from "mousePressed". 
+ */
+import processing.serial.*;
+
+Serial mySerial;
+
+float posx = 0;
+float posy = 0;
+float posz = 0;
+
+float previousx = posx;
+float previousy = posy;
+float previousz= 0;
+
+float translatex = 0;
+float translatey =0;
+float translatez = 0;
+
+boolean start = false;
+
+void setup() 
+{
+  size(900, 900);
+  noSmooth();
+
+
+  mySerial = new Serial(this, "COM10", 500000);
+  mySerial.clear();
+
+  stroke(0, 0, 240);
+  strokeWeight(5);
+  translate(450, 450);
+}
+
+int arrayindex = 0;
+
+void draw()
+{
+  if (mySerial.available() > 0)
+  {    
+    String value = mySerial.readStringUntil(10); //lf
+    if (value != null) {
+      try {
+        //value = value.substring(0, value.length()-1);
+        String[] splitted = split(value, ' ');
+        previousx = posx;
+        previousy = posy;
+        previousz = posz;
+        /*
+      posx = Integer.valueOf(splitted[0]);
+         posy = - Integer.valueOf(splitted[1]);
+         posz = Integer.valueOf(splitted[2]);*/
+        posx = float(splitted[0]) / 10;
+        posy = - float(splitted[1]) / 10;
+        posz = float(splitted[2]) / 400;
+      }
+      catch(Exception e) {
+        print("error : ");
+        print(value);
+        print(e);
+      }
+    }
+  }
+  if (start) {
+    line(posx - translatex + 450, posy - translatey + 450, previousx - translatex + 450, previousy - translatey +450);
+    stroke(240, 0, 0);
+    lineAngle(800, 800, posz, float(50));
+    stroke(0,0,240);
+  } else {
+    posx=0;
+    posy = 0;
+    previousx = 0;
+    previousy=0;
+    background(200);
+  }
+}
+
+void keyPressed()
+{
+  start = true;
+  translatex = posx;
+  translatey = posy;
+  translatez = posz;
+  background(200);
+
+  stroke(250, 0, 0);
+  ellipse(800, 800, 50, 50);
+  stroke(0,0,240);
+}
+
+void lineAngle(int x, int y, float angle, float length)
+{
+  line(x, y, x+cos(angle+90)*length, y-sin(angle+90)*length);
+}
